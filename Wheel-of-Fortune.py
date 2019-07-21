cd31f31ba15e220c10591f226979682815e22b10
DA2017 <31857664+subassk@users.noreply.github.com>
2019-07-21 13:51:24 +0530

    Update Wheel-of-Fortune.py

--- Wheel-of-Fortune.py
+++ Wheel-of-Fortune.py
@@ -1,1 +1,29 @@
+import time
+
+for x in range(2, 6):
+    print('Sleep {} seconds..'.format(x))
+    time.sleep(x) # "Sleep" for x seconds
+print('Done!')
+
+
+
+import random
+
+rand_number = random.randint(1, 10)
+print('Random number between 1 and 10: {}'.format(rand_number))
+
+letters = [letter for letter in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+rand_letter = random.choice(letters)
+print('Random letter: {}'.format(rand_letter))
+
+
+myString = 'Hello, World! 123'
+
+print(myString.upper()) # HELLO, WORLD! 123
+print(myString.lower()) # hello, world! 123
+print(myString.count('l')) # 3
+
+s = 'python is pythonic'
+print(s.count('python')) # 2
+
 
